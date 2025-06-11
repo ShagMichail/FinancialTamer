133caeca52bd656e2fcb140c5379e84427ecd3db
ShagMichail <shiga2001@mail.ru>
2025-06-11 13:49:53 +0300

    Initial Commit

---
+++ FinancialTamer/FinancialTamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTamerApp.swift
+//  FinancialTamer
+//
+//  Created by Михаил Шаговитов on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
