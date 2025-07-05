86520b002ef313b5d88a13f74889078b269d338e
ShagMichail <shiga2001@mail.ru>
2025-07-05 03:33:10 +0300

    Completed homework 4

---
+++ FinancialTamer/Modules/Articles/ArticlesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArticlesViewModel.swift
+//  FinancialTamer
+//
+//  Created by Михаил Шаговитов on 02.07.2025.
+//
+
+import Foundation
---
+++ FinancialTamer/Modules/CustomViews/SearchBar.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBar.swift
+//  FinancialTamer
+//
+//  Created by Михаил Шаговитов on 05.07.2025.
+//
+
+import Foundation
