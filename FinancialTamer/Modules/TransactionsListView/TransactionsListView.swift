bdbf94b51807082f7bc87f9d748dec9c63ae2a9e
ShagMichail <shiga2001@mail.ru>
2025-07-05 03:40:22 +0300

    Fix

--- FinancialTamer/Modules/TransactionsListView/TransactionsListView.swift
+++ FinancialTamer/Modules/TransactionsListView/TransactionsListView.swift
@@ -51,9 +51,6 @@ struct TransactionsListView: View {
                             )
                         }
                         .listRowInsets(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-                        .alignmentGuide(.listRowSeparatorLeading) { viewDimensions in
-                            return viewDimensions[.listRowSeparatorLeading] + 46
-                        }
                     }
                     
                 } header: {
